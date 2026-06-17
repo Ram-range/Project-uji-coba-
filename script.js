fe5da2cd20a0b23e2a44eafba338805141e936b6
Daniel <dd0201449@gmail.com>
2026-06-17 12:19:48 +0800

    Add user greeting functionality in script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// Ubah nama user
+let namaUser = prompt("Masukkan nama Anda:");
+if (namaUser) {
+    document.getElementById("namaUser").innerText = namaUser;
+}
+
+// Tombol sapa
+document.getElementById("salamBtn").addEventListener("click", function() {
+    let nama = document.getElementById("namaUser").innerText;
+    alert("Halo " + nama + "! Semangat terus belajarnya ya! 💪");
+});
